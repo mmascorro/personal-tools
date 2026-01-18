78e4a0db15d26e52de0d0aa44f1007869df33780
Miguel Mascorro <futuretuesday@gmail.com>
2026-01-18 00:44:54 -0600

    update db engine string

--- personaltools/settings/dev.py
+++ personaltools/settings/dev.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'personaltools',
         'USER': 'personaltools',
         'PASSWORD': 'personaltools',
